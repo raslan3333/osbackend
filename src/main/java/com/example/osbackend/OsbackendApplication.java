f724ff41c39de4158f5642013c3418472349a7ff
raslan3333 <raslan3333@gmail.com>
2020-12-08 14:00:20 +0100

    Initial commit

---
+++ src/main/java/com/example/osbackend/OsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.osbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsbackendApplication.class, args);
+	}
+
+}
