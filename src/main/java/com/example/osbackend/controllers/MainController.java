a49b764c0993fd5c465428d0baf3a3cb8010ffe4
raslan3333 <raslan3333@gmail.com>
2020-12-08 16:04:06 +0100

    controller test

---
+++ src/main/java/com/example/osbackend/controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.example.osbackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello World";
+    }
+}
